fef9363ab69ddd0e9346e8a0ddea76e82b1b251b
Caleb Chaney <calebc347@gmail.com>
2020-12-03 10:39:52 -0600

    Updated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,22 +9,18 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png",
 ];
 
+
 // install
 self.addEventListener("install", function (evt) {
-    // pre cache image data
     evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
-    // pre cache all static assets
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
-    // tell the browser to activate this service worker immediately once it
-    // has finished installing
     self.skipWaiting();
 });
 
-// activate
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
@@ -43,6 +39,7 @@ self.addEventListener("activate", function (evt) {
 
 // fetch
 self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
     if (evt.request.url.includes("/api/")) {
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
@@ -63,11 +60,11 @@ self.addEventListener("fetch", function (evt) {
         return;
     }
 
+    // if the request is not for the API, serve static assets using "offline-first" approach.
     evt.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.match(evt.request).then(response => {
-                return response || fetch(evt.request);
-            });
+        caches.match(evt.request).then(function (response) {
+            return response || fetch(evt.request);
         })
     );
 });
+
